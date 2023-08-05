d2ec0e62e09772b5e674ba07caf7af1a77275ab2
Saahil Claypool <saahil@claypools.org>
2023-08-05 19:12:57 -0400

    use dynamic tuple

--- src/TagHelpers.cs
+++ src/TagHelpers.cs
@@ -3,21 +3,32 @@ namespace HtmlTagHelpers;
 public static partial class TagHelpers
 {
   public static readonly object? NO_VALUE = null;
+
   public static HtmlTag H(string tag, params object[] childrenOrProps)
   {
     List<(string Key, object? Value)> props = new(childrenOrProps.Length);
     List<object> childrenItems = new(childrenOrProps.Length);
     foreach (var item in childrenOrProps)
     {
-      if (item is ValueTuple<string, object?> tuple)
-        props.Add(tuple);
+      var itemType = item.GetType();
+      // since this is dotet standard, can't simply write
+      // if (item is var (key, value))
+      if (
+        itemType.Name.StartsWith("ValueTuple`")
+        && itemType.IsValueType
+        && itemType.IsGenericType
+        && itemType.GenericTypeArguments.Count() == 2
+      )
+      {
+        dynamic dyn = item;
+        props.Add(((string)dyn.Item1, (object?)dyn.Item2));
+      }
       else
         childrenItems.Add(item);
     }
     return new(tag, new HData(props), childrenItems);
-    
   }
-  
+
   public static HtmlTag If(bool condition, HtmlTag ifTrue, HtmlTag ifFalse) =>
     condition ? ifTrue : ifFalse;
 }
